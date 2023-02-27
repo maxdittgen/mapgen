e552cafa6e6ec2de9184befbb06c5ff1240330b1
Maximilian Dittgen <myd4@cornell.edu>
2023-02-27 12:32:24 -0500

    Implemented mapstyles.py
    - Support enum types for custom-made mapbox styles

---
+++ mapstyles.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class MapStyles(Enum):
+    SATELLITE = "mapbox/satellite-v9"
+    BROWN = "mdittgen/clen0bgnk003601phy50oham4"
+    BLUEPRINT = "mdittgen/clen2968d00h101qx9vftn39v"
+    BLUEMONO = "mdittgen/clen2pj24002001o2d5v6orvn"
+    
