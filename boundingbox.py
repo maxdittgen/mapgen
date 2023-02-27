6abcb88638be263e91cdbe058f1be0f511e41105
Maximilian Dittgen <myd4@cornell.edu>
2023-02-27 14:06:52 -0500

    implemented __str__ method

--- boundingbox.py
+++ boundingbox.py
@@ -4,3 +4,7 @@ class BoundingBox:
         self.lowlat = lowlat
         self.uplong = uplong
         self.uplat = uplat
+
+    def __str__(self):
+        return ("[(" + str(self.lowlat) + ", " + str(self.lowlong) +
+                ") , (" + str(self.uplat) + ", " + str(self.uplong) + ")]")
