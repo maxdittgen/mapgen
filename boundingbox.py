4d423c10d32463add7be77268a59a6e6dfcfd10e
Maximilian Dittgen <myd4@cornell.edu>
2023-02-27 13:52:13 -0500

    updated parameter names

--- boundingbox.py
+++ boundingbox.py
@@ -1,5 +1,5 @@
 class BoundingBox:
-    def __init__(self, llong, llat, ulong, ulat):
+    def __init__(self, lowlong, lowlat, uplong, uplat):
         self.lowlong = llong
         self.lowlat = llat
         self.uplong = ulong
