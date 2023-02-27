a27a564a2f6230aabeb293971b2cc2c7f6d1af93
Maximilian Dittgen <myd4@cornell.edu>
2023-02-27 13:57:04 -0500

    implemented get_bounding_box
    - added comments for requirements and returns

--- retriever.py
+++ retriever.py
@@ -2,12 +2,15 @@ import requests
 import cv2
 import numpy as np
 from boundingbox import BoundingBox
+import json
 from mapstyles import MapStyles
 
 
 class Retriever:
     api_key = "pk.eyJ1IjoibWRpdHRnZW4iLCJhIjoiY2xlbjBiYjNmMHFkbTN1czBia3BiYzJ4NSJ9.OkLtkJ82TkrpkyzF4YmjeQ"
 
+    # given an enum MapStyle, float width and height resolution, and BoundingBox object of
+    # coordinates, returns an opencv-readable image of the given tile
     def get_maptile(style, width, height, bbox):
         bbox_string = '[' + str(bbox.lowlong) + ',' + str(bbox.lowlat) + \
             ',' + str(bbox.uplong) + ',' + str(bbox.uplat) + ']'
@@ -19,3 +22,25 @@ class Retriever:
         mapimage = np.asarray(bytearray(resp.read()), dtype="uint8")
         mapimage = cv2.imdecode(mapimage, cv2.IMREAD_COLOR)
         return mapimage
+
+    # Requires:
+    # - searchloc: a search string representing a location of center of map
+    # - width: float representing number of miles the image is wide
+    # - aspect: float representing width divided by height of desired map
+    # Returns: Bounding box object
+    def get_bounding_box(searchloc, width, aspect):
+        search = searchloc.replace(" ", "%20")
+        resp = requests.get("https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+                            search + ".json?proximity=ip&access_token=" +
+                            Retriever.api_key)
+        data = resp.json()
+
+        # edit this to get full list of possibilities later on
+        loc_coords = ((data['features'])[0])['center']
+
+        heightlat = (width / aspect) / 69.0
+        widthlong = width / 54.6
+        return BoundingBox(loc_coords[1] - (widthlong / 2),
+                           loc_coords[0] - (heightlat / 2),
+                           loc_coords[1] + (widthlong / 2),
+                           loc_coords[0] + (heightlat / 2))
