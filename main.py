9a9f79cbd4da613286e789167db402cf14bde13d
Maximilian Dittgen <myd4@cornell.edu>
2023-02-27 22:51:44 -0500

    added templates and images - added favicons - created layouts.html - initialized flask in main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import cv2
+# imports
+from flask import Flask, render_template, request, redirect, url_for, session
 from retriever import Retriever
 from mapstyles import MapStyles
-from boundingbox import BoundingBox
 from framemaker import FrameMaker
+
+# init flask
+app = Flask(__name__)
 
 # given a mapstyle enum, place search string, diameter (in miles), and width and
 # height in inches, generates a 300ppi png of the place
@@ -18,23 +21,13 @@ def make_poster(style, place, diameter, width, height):
     frame.frame.show()
 
 
-make_poster(MapStyles.BLUEPRINT, "Cornell University, NY", 2, 17, 12)
+make_poster(MapStyles.BLUEPRINT, "Cornell University, NY", 2, 4, 3)
+
+# routing
+
+# routing to main page
 
 
-# im = Retriever.get_map(MapStyles.BLUEPRINT, "Pittsburgh, PA", 5, 9.2, 7)
-# cv2.imshow('map', im)
-# cv2.waitKey(0)
-
-# f1 = FrameMaker(10, 10)
-# f1.addText("New York", "a set of coordinates")
-
-# Retriever.get_map(MapStyles.BLUEPRINT, "515 E 89th St", 5, 4, 5)
-
-# im = Retriever.get_map_test(MapStyles.BLUEPRINT, "515 E 89th St", 5, 720, 480)
-# cv2.imshow('map', im)
-# cv2.waitKey(0)
-
-# im = Retriever.get_maptile(MapStyles.SATELLITE, 1240, 1240,
-#                            Retriever.get_bounding_box("515 E 89th St, New York", 0.5, 0.5))
-# cv2.imshow('map', im)
-# cv2.waitKey(0)
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    return render_template('index.html')
