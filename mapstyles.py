6a22229b8005e05ac445384eec89947ac1748a72
Maximilian Dittgen <myd4@cornell.edu>
2023-02-27 21:16:21 -0500

    add white style

--- mapstyles.py
+++ mapstyles.py
@@ -6,3 +6,4 @@ class MapStyles(Enum):
     BROWN = "mdittgen/clen0bgnk003601phy50oham4"
     BLUEPRINT = "mdittgen/clen2968d00h101qx9vftn39v"
     BLUEMONO = "mdittgen/clen2pj24002001o2d5v6orvn"
+    WHITE = "mdittgen/clenm4xnk002l01n668wwf086"
